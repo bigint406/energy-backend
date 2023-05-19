234211430619fb3dc7e40ff2e0b34489208b7e55
zengshuang <1084973153@qq.com>
2023-05-19 10:38:35 +0800

    更新发电量

--- model/solarElec.go
+++ model/solarElec.go
@@ -1,9 +1,13 @@
 package model
 
 func CalcSolarElecGenHour(hourStr string) float64 {
-	l, ok := GetOpcFloatList("ZLZ.%E6%80%BB%E5%8F%91%E7%94%B5%E9%87%8F1", hourStr) //总发电量
+	l, ok := GetOpcFloatList("ZLZ.%E6%80%BB%E5%8F%91%E7%94%B5%E9%87%8F1", hourStr) //总发电量1
 	if !ok {
 		return 0
 	}
-	return l[len(l)-1] - l[0]
+	l2, ok := GetOpcFloatList("ZLZ.%E6%80%BB%E5%8F%91%E7%94%B5%E9%87%8F", hourStr) //总发电量
+	if !ok {
+		return 0
+	}
+	return RightSubLeft(l) + RightSubLeft(l2)
 }
